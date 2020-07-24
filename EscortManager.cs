f4288441b3f9c9182ade65a4bc52bb600da26be5
computeraims <47012776+computeraims@users.noreply.github.com>
2020-07-23 21:42:46 -0500

    Removed custom command handler, added radius scan over raytrace

--- EscortManager.cs
+++ EscortManager.cs
@@ -1,5 +1,4 @@
-﻿using Escort.Models;
-using HarmonyLib;
+﻿using HarmonyLib;
 using SDG.Unturned;
 using Steamworks;
 using System;
@@ -13,51 +12,23 @@ namespace Escort
 {
     class EscortManager : MonoBehaviour
     {
-        public HyperCommand EscortCommand;
         public static Dictionary<CSteamID, CSteamID> EscortingPlayers;
         public static Dictionary<CSteamID, CSteamID> EscortedPlayers;
-        public HyperCommand LoadCommand;
-        public HyperCommand UnloadCommand;
 
         public void Awake()
         {
             Console.WriteLine("EscortManager loaded");
-            ChatManager.onChatted += OnChatted;
+
             PlayerLife.onPlayerDied += OnPlayerDied;
             Provider.onEnemyDisconnected += OnPlayerDisconnected;
 
             EscortingPlayers = new Dictionary<CSteamID, CSteamID>();
             EscortedPlayers = new Dictionary<CSteamID, CSteamID>();
 
-            EscortCommand = new CommandEscort();
-            LoadCommand = new CommandLoad();
-            UnloadCommand = new CommandUnload();
-        }
+            Commander.register(new CommandEscort());
+            Commander.register(new CommandLoad());
+            Commander.register(new CommandUnload());
 
-        private void OnChatted(SteamPlayer player, EChatMode Mode, ref Color Color, ref bool isRich, string text, ref bool isVisible)
-        {
-            if (text[0] == '/')
-            {
-                isVisible = false;
-                string[] InputArray = text.Split(' ');
-                InputArray[0] = InputArray[0].Substring(1);
-                string[] args = InputArray.Skip(1).ToArray();
-
-                if (InputArray[0].ToLower() == "escort")
-                {
-                    EscortCommand.execute(player.playerID.steamID, args);
-                }
-
-                if (InputArray[0].ToLower() == "load")
-                {
-                    LoadCommand.execute(player.playerID.steamID, args);
-                }
-
-                if (InputArray[0].ToLower() == "unload")
-                {
-                    UnloadCommand.execute(player.playerID.steamID, args);
-                }
-            }
         }
 
         private void OnPlayerDied(PlayerLife sender, EDeathCause cause, ELimb limb, CSteamID instigator)
@@ -163,76 +134,87 @@ namespace Escort
             }
         }
 
-        public class CommandEscort : HyperCommand
+        public class CommandEscort : Command
         {
-            public CommandEscort()
+            protected override void execute(CSteamID executorID, string parameter)
             {
-                Name = "escort";
-                Description = "Escort a restrained player";
-                Usage = "";
-            }
-
-            public override void execute(CSteamID executor, string[] args)
-            {
-                if (EscortingPlayers.ContainsKey(executor))
+                if (EscortingPlayers.ContainsKey(executorID))
                 {
                     CSteamID escorteeID;
-                    if (EscortingPlayers.TryGetValue(executor, out escorteeID))
+                    if (EscortingPlayers.TryGetValue(executorID, out escorteeID))
                     {
                         EscortedPlayers.Remove(escorteeID);
                         Player ply = PlayerTool.getPlayer(escorteeID);
                         ply.movement.sendPluginSpeedMultiplier(Main.Config.arrestMoveSpeed);
                     }
 
-                    EscortingPlayers.Remove(executor);
+                    EscortingPlayers.Remove(executorID);
                     return;
-                } else if (EscortedPlayers.ContainsKey(executor))
+                }
+                else if (EscortedPlayers.ContainsKey(executorID))
                 {
                     return;
 
                 }
 
-                SteamPlayer player = PlayerTool.getSteamPlayer(executor);
-                RaycastInfo thingLocated = TraceRay(player, 20f, RayMasks.PLAYER | RayMasks.PLAYER_INTERACT);
+                SteamPlayer player = PlayerTool.getSteamPlayer(executorID);
 
-                if (thingLocated.player != null && thingLocated.player.animator.gesture == EPlayerGesture.ARREST_START)
+                List<Player> nearbyPlayers = new List<Player>();
+
+                PlayerTool.getPlayersInRadius(player.player.movement.transform.position, 10f, nearbyPlayers);
+
+                if (nearbyPlayers == null)
                 {
-                    EscortingPlayers.Add(player.playerID.steamID, thingLocated.player.channel.owner.playerID.steamID);
-                    EscortedPlayers.Add(thingLocated.player.channel.owner.playerID.steamID, player.playerID.steamID);
-                    thingLocated.player.movement.sendPluginSpeedMultiplier(0); // make it so players cant move while being escorted
+                    return;
                 }
+
+                foreach (Player p in nearbyPlayers)
+                {
+                    if (p.animator.gesture == EPlayerGesture.ARREST_START)
+                    {
+                        EscortingPlayers.Add(player.playerID.steamID, p.channel.owner.playerID.steamID);
+                        EscortedPlayers.Add(p.channel.owner.playerID.steamID, player.playerID.steamID);
+                        p.movement.sendPluginSpeedMultiplier(0); // make it so players cant move while being escorted
+                        return;
+                    }
+                }
+            }
+
+            public CommandEscort()
+            {
+                this.localization = new Local();
+                this._command = "escort";
+                this._info = "escort";
+                this._help = "Escort handcuffed players";
             }
         }
 
-        public class CommandLoad : HyperCommand
+        public class CommandLoad : Command
         {
-            public CommandLoad()
+            protected override void execute(CSteamID executorID, string parameter)
             {
-                Name = "load";
-                Description = "Load an escorted player into a vehicle";
-                Usage = "";
-            }
-            public override void execute(CSteamID executor, string[] args)
-            {
-                SteamPlayer player = PlayerTool.getSteamPlayer(executor);
-                RaycastInfo thingLocated = TraceRay(player, 20f, RayMasks.VEHICLE);
+                SteamPlayer player = PlayerTool.getSteamPlayer(executorID);
 
-                if (thingLocated.vehicle != null)
+                List<InteractableVehicle> nearbyVehicles = new List<InteractableVehicle>();
+
+                VehicleManager.getVehiclesInRadius(player.player.movement.transform.position, 15f, nearbyVehicles);
+
+                foreach (InteractableVehicle v in nearbyVehicles)
                 {
                     byte seat;
                     CSteamID escorteeID;
 
-                    if (EscortingPlayers.TryGetValue(executor, out escorteeID))
+                    if (EscortingPlayers.TryGetValue(executorID, out escorteeID))
                     {
                         Player ply = PlayerTool.getPlayer(escorteeID);
 
 
-                        if (thingLocated.vehicle.tryAddPlayer(out seat, ply))
+                        if (v.tryAddPlayer(out seat, ply) && !v.isLocked)
                         {
                             EscortedPlayers.Remove(escorteeID);
-                            EscortingPlayers.Remove(executor);
+                            EscortingPlayers.Remove(executorID);
                             VehicleManager.instance.channel.send("tellEnterVehicle", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[] {
-                                thingLocated.vehicle.instanceID,
+                                v.instanceID,
                                 seat,
                                 escorteeID
                             });
@@ -240,38 +222,45 @@ namespace Escort
                     }
                 }
             }
+
+            public CommandLoad()
+            {
+                this.localization = new Local();
+                this._command = "load";
+                this._info = "load";
+                this._help = "Load an escorting player into a vehicle";
+            }
         }
 
-        public class CommandUnload : HyperCommand
+        public class CommandUnload : Command
         {
-            public CommandUnload()
+            protected override void execute(CSteamID executorID, string parameter)
             {
-                Name = "unload";
-                Description = "Unload an escorted player from a vehicle";
-                Usage = "";
-            }
+                SteamPlayer player = PlayerTool.getSteamPlayer(executorID);
 
-            public override void execute(CSteamID executor, string[] args)
-            {
-                SteamPlayer player = PlayerTool.getSteamPlayer(executor);
-                RaycastInfo thingLocated = TraceRay(player, 20f, RayMasks.VEHICLE);
+                List<InteractableVehicle> nearbyVehicles = new List<InteractableVehicle>();
 
-                if (thingLocated.vehicle != null && !thingLocated.vehicle.isLocked)
+                VehicleManager.getVehiclesInRadius(player.player.movement.transform.position, 15f, nearbyVehicles);
+
+                foreach (InteractableVehicle v in nearbyVehicles)
                 {
-                    for (int i = 0; i < thingLocated.vehicle.passengers.Length; i++)
+                    if (!v.isLocked)
                     {
-                        Passenger passenger = thingLocated.vehicle.passengers[i];
-                        if (passenger != null)
+                        for (int i = 0; i < v.passengers.Length; i++)
                         {
-                            SteamPlayer ply = passenger.player;
-                            if (ply != null)
+                            Passenger passenger = v.passengers[i];
+                            if (passenger != null)
                             {
-                                Player player2 = ply.player;
-                                if (!(player2 == null) && !player2.life.isDead)
+                                SteamPlayer ply = passenger.player;
+                                if (ply != null)
                                 {
-                                    if (ply.player.animator.gesture == EPlayerGesture.ARREST_START)
+                                    Player player2 = ply.player;
+                                    if (!(player2 == null) && !player2.life.isDead)
                                     {
-                                        VehicleManager.forceRemovePlayer(ply.playerID.steamID);
+                                        if (ply.player.animator.gesture == EPlayerGesture.ARREST_START)
+                                        {
+                                            VehicleManager.forceRemovePlayer(ply.playerID.steamID);
+                                        }
                                     }
                                 }
                             }
@@ -279,11 +268,14 @@ namespace Escort
                     }
                 }
             }
-        }
 
-        public static RaycastInfo TraceRay(SteamPlayer player, float distance, int masks)
-        {
-            return DamageTool.raycast(new Ray(player.player.look.aim.position, player.player.look.aim.forward), distance, masks);
+            public CommandUnload()
+            {
+                this.localization = new Local();
+                this._command = "unload";
+                this._info = "unload";
+                this._help = "Unload a restrained player from a vehicle";
+            }
         }
     }
 }
